4625d4bb81c3951933d1574dccc8f6d45586036f
MalayCreates <malayagarwal@outlook>
2024-02-01 09:00:05 -0500

    init

---
+++ lua/mega/init.lua
@@ -0,0 +1,7 @@
+require("mega.remap")
+vim.opt.colorcolumn = "120"
+require("mega.set")
+print("Welcome to your NVIM Configs! :]")
+vim.o.background = "dark" -- or "light" for light mode
+vim.cmd([[colorscheme gruvbox]])
+
