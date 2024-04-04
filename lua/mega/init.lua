2d0d3d07bc7bcd2163cb4459746effecdd60ca3b
MalayCreates <malayagarwal@outlook>
2024-04-04 15:10:49 -0400

    added live grep

--- lua/mega/init.lua
+++ lua/mega/init.lua
@@ -1,7 +1,6 @@
 require("mega.remap")
 vim.opt.colorcolumn = "120"
 require("mega.set")
-print("Welcome to your NVIM Configs! :]")
 vim.o.background = "dark" -- or "light" for light mode
 vim.cmd([[colorscheme gruvbox]])
 
